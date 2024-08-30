a06f301a232ef121adea8a5afd304018677c83b6
imparfait <155664027+imparfait@users.noreply.github.com>
2024-08-30 19:38:57 +0300

    add class Worker

---
+++ ado.net/05_fluentAPI/classes/Position.cs
@@ -0,0 +1,17 @@
+﻿using _05_fluentAPI.classes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_fluentAPI.tables
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Worker> Workers { get; set; }
+    }
+}
---
+++ ado.net/05_fluentAPI/classes/Worker.cs
@@ -0,0 +1,24 @@
+﻿using _05_fluentAPI.tables;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_fluentAPI.classes
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public decimal Salary { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public int PositionId { get; set; }
+        public Position Position { get; set; }
+        public int ShopId { get; set; }
+        public Shop Shop { get; set; }
+    }
+}
