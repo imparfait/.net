a172cb5da12d59aeccc2116ea7d016fb7f5e993c
imparfait <155664027+imparfait@users.noreply.github.com>
2024-08-30 19:18:53 +0300

    створення класу Positions

---
+++ ado.net/05_fluentAPI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05_fluentAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
